eb775ff3c5f57cb01c492d4c12120f713be22bc9
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-02-28 12:04:50 +0100

    1

--- main.R
+++ main.R
@@ -7,22 +7,22 @@
 # path_to_scripts <- "scripts/"
 #
 # # Analyse exploratoire des données
-# source(paste0(path_to_scripts, "01_exploration.R"))
+
 #
 # # Identification et catégorisation des véhicules
-# source(paste0(path_to_scripts, "02_categorisation.R"))
+
 #
 # # Application des catégories et fusion des données
-# source(paste0(path_to_scripts, "03_application_et_fusion.R"))
+
 #
 # # Modélisation - Création du modèle de classification
-# source(paste0(path_to_scripts, "04_modélisation.R"))
+
 #
 # # Évaluation du modèle
-# source(paste0(path_to_scripts, "05_évaluation.R"))
+
 #
 # # Application aux données marketing
-# source(paste0(path_to_scripts, "06_application_marketing.R"))
+
 #
 # # Conclusion et génération de rapport
 # source(paste0(path_to_scripts, "07_rapport.R"))
