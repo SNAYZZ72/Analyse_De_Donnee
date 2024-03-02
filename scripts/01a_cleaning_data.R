87e96ca35579281a1db127af6aecf3efb89da836
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-03-02 15:37:22 +0100

    cleaning via cloud corrigé

--- scripts/01a_cleaning_data.R
+++ scripts/01a_cleaning_data.R
@@ -46,7 +46,6 @@ charger_et_nettoyer <- function(nom_table) {
       df$X2eme.voiture <- filter(nchar(df$X2eme.voiture) > 0)
   }
 
-  #"^[0-9]{4} [A-Z]{2} [0-9]{2}$"
   if("immatriculation" %in% names(df)) {
     df$immatriculation <- str_detect(immatriculation, "^[0-9]{4} [A-Z]{2} [0-9]{2}$")
     }
