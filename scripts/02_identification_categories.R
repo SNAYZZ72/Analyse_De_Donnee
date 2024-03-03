f036532a17fdfe70e4a0f7a7fd4cb04e25d49e2d
ayakinani <aya.kinani@etu.estia.fr>
2024-03-03 16:54:40 +0100

    id_cat modifiee

--- scripts/02_identification_categories.R
+++ scripts/02_identification_categories.R
@@ -1,20 +1,13 @@
-#--creation de 6 categories (citadine, compact, routière, familiale, sportive, berline)
+# Importation des données depuis le fichier catalogue_clean.csv
+catalogue <- read.csv("C:\\data_analytics\\data\\cleaned\\Catalogue_clean.csv")
 
+# Code pour identifier les catégories de véhicules à partir du catalogue
 catalogue$categories <- ifelse(catalogue$longueur=="courte","citadine",
                                ifelse(catalogue$longueur=="moyenne","compacte",
-                                      ifelse(catalogue$longueur=="longue"& catalogue$nbPlaces== 5& catalogue$puissance<180,"routi?re",
+                                      ifelse(catalogue$longueur=="longue"& catalogue$nbPlaces== 5& catalogue$puissance<180,"routière",
                                              ifelse(catalogue$longueur=="longue"&catalogue$nbPlaces== 7&catalogue$puissance<180,"familiale",
                                                     ifelse(catalogue$longueur=="longue" | catalogue$longueur=="très longue" & catalogue$puissance >180 & catalogue$puissance <300,"sportive",
                                                            ifelse(catalogue$longueur == "très longue" & catalogue$puissance >300, "berline","rien"))))))
 
-
-#------ applications au fichiers Immatriculation.csv
-
-
-
-immatriculation$categories <- ifelse(immatriculation$longueur=="courte","citadine",
-                                     ifelse(immatriculation$longueur=="moyenne","compacte",
-                                            ifelse(immatriculation$longueur=="longue"& immatriculation$nbPlaces== 5& immatriculation$puissance<180,"routière",
-                                                   ifelse(immatriculation$longueur=="longue"&immatriculation$nbPlaces== 7&immatriculation$puissance<180,"familiale",
-                                                          ifelse(immatriculation$longueur=="longue" | immatriculation$longueur=="très longue" & immatriculation$puissance >180 & immatriculation$puissance <300,"sportive",
-                                                                 ifelse(immatriculation$longueur == "très longue" & immatriculation$puissance >300, "berline","rien"))))))
+# Affichage des premières lignes du catalogue avec les catégories ajoutées
+head(catalogue)
