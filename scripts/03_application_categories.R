c4474d3af2bf0c84e11f17d99fb8cf14da5959fd
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-03-10 20:35:25 +0100

    correction on a bien 270 vehicule

--- scripts/03_application_categories.R
+++ scripts/03_application_categories.R
@@ -1,14 +1,17 @@
 # Application des catégorie identifier dans le script 02_identification_categories.R
 # Vérification et installation des packages nécessaires
-packages_needed <- c("cluster", "ggplot2", "dplyr")
+packages_needed <- c("cluster", "ggplot2", "dplyr", "readr", "data.table", "scales")
 packages_to_install <- packages_needed[!(packages_needed %in% installed.packages()[,"Package"])]
 if(length(packages_to_install)) install.packages(packages_to_install)
 
 # Chargement des packages
-library(cluster)
-library(ggplot2)
-library(dplyr)
-library(readr)
+# library(cluster)
+# library(ggplot2)
+# library(dplyr)
+# library(readr)
+# library(data.table)
+# library(scales)
+lapply(packages_needed, library, character.only = TRUE)
 
 # Chemin vers les données nettoyées
 chemin_data <- "data/cleaned"
@@ -20,8 +23,7 @@ load_data <- function(file_name) {
     message("Le fichier ", data_path, " n'existe pas.")
     return(NULL)
   }
-  data <- read.csv(data_path, header = TRUE, sep = ",", stringsAsFactors = FALSE, encoding = "UTF-8")
-
+  data <- fread(data_path, encoding = "UTF-8")
   return(data)
 }
 
@@ -30,16 +32,21 @@ catalogue <- load_data("Catalogue_sans_accents_clean.csv")
 
 # Fonction pour attribuer une catégorie
 attribuer_categorie_complexe <- function(catalogue) {
+  # Assurer que 'longueur' et 'puissance' sont au format correct (si nécessaire)
+  catalogue$longueur <- as.character(catalogue$longueur)
+  catalogue$puissance <- as.numeric(as.character(catalogue$puissance))
+
+  # Application des catégories en fonction de critères prédéfinis
   catalogue <- catalogue %>%
     mutate(Categorie = case_when(
-        longueur == "courte" ~ "Citadines compactes et abordables",
-        longueur == "moyenne" ~ "Berlines familiales puissantes et confortables",
-        longueur == "longue" & nbPlaces == 5 & puissance < 180 ~ "Voitures compactes sportives et economiques",
-        longueur == "longue" & nbPlaces == 7 & puissance < 180 ~ "Monospaces familiaux spacieux et abordables",
-        (longueur == "longue" | longueur == "tres longue") & puissance > 180 & puissance < 300 ~ "Voitures de sport puissantes et luxueuses",
-        longueur == "tres longue" & puissance > 300 ~ "Autres",
-        TRUE ~ "Non spécifié"
-        ))
+      longueur == "courte" & puissance <= 120 ~ "Citadines compactes et abordables",
+      longueur %in% c("moyenne", "longue") & puissance >= 120 & puissance <= 180 ~ "Berlines familiales puissantes et confortables",
+      longueur == "longue" & nbPlaces <= 5 & puissance <= 250 ~ "Voitures compactes sportives et economiques",
+      longueur == "longue" & nbPlaces > 5 & puissance <= 180 ~ "Monospaces familiaux spacieux et abordables",
+      longueur %in% c("longue", "tres longue") & puissance > 180 & puissance <= 300 ~ "Voitures de sport puissantes et luxueuses",
+      longueur == "tres longue" & puissance > 300 ~ "Autres",
+      TRUE ~ "Non specifie"
+    ))
   return(catalogue)
 }
 
@@ -50,9 +57,15 @@ catalogue <- attribuer_categorie_complexe(catalogue)
 head(catalogue, 50)
 
 # Affichez la distribution des catégories
-ggplot(catalogue, aes(x = Categorie)) +
+ggplot(catalogue, aes(x = factor(Categorie), fill = Categorie)) +
   geom_bar() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+  geom_text(stat = 'count', aes(label = ..count..), vjust = -0.5, color = "black") +
+  scale_fill_viridis_d() +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 64, hjust = 1)) +
+  labs(title = "Distribution des Categories de Vehicules", x = "Categorie", y = "Nombre de Vehicules")
+
+
 
 # Enregistrer le catalogue mis à jour dans un nouveau fichier CSV
 # write.csv(catalogue, "data/cleaned/Catalogue_avec_categories.csv", row.names = FALSE)
