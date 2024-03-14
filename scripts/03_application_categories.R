26513721b360c169267d16821b9c3c619b95a40c
matthieu.picot <matthieu.picot@etu.estia.fr>
2024-03-14 16:05:24 +0100

    test

--- scripts/03_application_categories.R
+++ scripts/03_application_categories.R
@@ -29,7 +29,6 @@ immatriculations$longueur <- factor(immatriculations$longueur, levels = c("court
 immatriculations$longueur <- as.numeric(as.character(immatriculations$longueur))
 
 
-
 immatriculations$prix <- as.numeric(as.character(immatriculations$prix))
 
 # Valeur test
