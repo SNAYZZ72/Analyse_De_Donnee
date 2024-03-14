9b38cd34922537055445526e50ee8b1cbfc5a5eb
matthieu.picot <matthieu.picot@etu.estia.fr>
2024-03-14 18:05:17 +0100

    fusion des données

--- scripts/04_fusion_donnees.R
+++ scripts/04_fusion_donnees.R
@@ -24,46 +24,15 @@ load_data <- function(file_name) {
 immatriculations <- load_data("Immatriculations_categorie.csv")
 
 # Charger les données clients
-clients <- load_data("Clients_7_sans_accents_clean.csv")
+clients7 <- load_data("Clients_7_sans_accents_clean.csv")
+clients12 <- load_data("Clients_12_sans_accents_clean.csv")
 
-# Assurez-vous que la colonne immatriculation est une chaîne de caractères dans les deux data frames
-immatriculations$immatriculation <- as.character(immatriculations$immatriculation)
-clients$immatriculation <- as.character(clients$immatriculation)
-
-# Supprimer les espaces potentiels avant ou après les immatriculations
-immatriculations$immatriculation <- trimws(immatriculations$immatriculation)
-clients$immatriculation <- trimws(clients$immatriculation)
-
-# Vérifier s'il y a des immatriculations uniques dans chaque jeu de données
-unique_immatriculations <- unique(immatriculations$immatriculation)
-unique_clients <- unique(clients$immatriculation)
-
-# Vérifier les immatriculations qui sont dans un ensemble mais pas dans l'autre
-missing_in_clients <- setdiff(unique_immatriculations, unique_clients)
-missing_in_immatriculations <- setdiff(unique_clients, unique_immatriculations)
-
-# Afficher les immatriculations manquantes
-print(missing_in_clients)
-print(missing_in_immatriculations)
+# Fusionner les données clients
+clients <- rbind(clients7, clients12)
 
 
-# Vérification des noms de colonnes pour trouver une clé de fusion commune
-# Remplacez 'immatriculation' par le nom de colonne réel qui représente la clé commune
-print(names(immatriculations))
-print(names(clients))
+# Fusionner les données mais on garde que les immatriculations qui ont des clients
+merged_data <- merge(clients, immatriculations, by = "immatriculation")
 
-# Fusion des données
-# 'immatriculation' est le nom de colonne commun utilisé pour la fusion
-donnees_fusionnees <- merge(immatriculations, clients, by = "immatriculation", all.x = TRUE)
-
-# Exploration des données fusionnées
-head(donnees_fusionnees)
-
-# Enregistrement des données fusionnées
-enregistrer_dataframe <- function(dataframe, file_name) {
-  data_path <- file.path(chemin_data, file_name)
-  write.csv(dataframe, file = data_path, row.names = FALSE, quote = FALSE)
-}
-
-# Enregistrer les données fusionnées
-enregistrer_dataframe(donnees_fusionnees, "Donnees_Fusionnees.csv")
+# on enregistre les données
+write.csv(merged_data, file = "data/cleaned/Donnees_Fusionnees.csv", row.names = FALSE)
