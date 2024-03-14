bd93814eca02b6ba2e5d30d22f95926d1b414191
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2024-03-14 17:20:20 +0100

    fusion test 2

--- scripts/04_fusion_donnees.R
+++ scripts/04_fusion_donnees.R
@@ -21,10 +21,31 @@ load_data <- function(file_name) {
 }
 
 # Charger les données d'immatriculations
-immatriculations <- load_data("Immatriculations_sans_accents_clean.csv")
+immatriculations <- load_data("Immatriculations_categorie.csv")
 
 # Charger les données clients
 clients <- load_data("Clients_7_sans_accents_clean.csv")
+
+# Assurez-vous que la colonne immatriculation est une chaîne de caractères dans les deux data frames
+immatriculations$immatriculation <- as.character(immatriculations$immatriculation)
+clients$immatriculation <- as.character(clients$immatriculation)
+
+# Supprimer les espaces potentiels avant ou après les immatriculations
+immatriculations$immatriculation <- trimws(immatriculations$immatriculation)
+clients$immatriculation <- trimws(clients$immatriculation)
+
+# Vérifier s'il y a des immatriculations uniques dans chaque jeu de données
+unique_immatriculations <- unique(immatriculations$immatriculation)
+unique_clients <- unique(clients$immatriculation)
+
+# Vérifier les immatriculations qui sont dans un ensemble mais pas dans l'autre
+missing_in_clients <- setdiff(unique_immatriculations, unique_clients)
+missing_in_immatriculations <- setdiff(unique_clients, unique_immatriculations)
+
+# Afficher les immatriculations manquantes
+print(missing_in_clients)
+print(missing_in_immatriculations)
+
 
 # Vérification des noms de colonnes pour trouver une clé de fusion commune
 # Remplacez 'immatriculation' par le nom de colonne réel qui représente la clé commune
@@ -33,7 +54,7 @@ print(names(clients))
 
 # Fusion des données
 # 'immatriculation' est le nom de colonne commun utilisé pour la fusion
-donnees_fusionnees <- merge(x = immatriculations, y = clients, by = "immatriculation", all = TRUE)
+donnees_fusionnees <- merge(immatriculations, clients, by = "immatriculation", all.x = TRUE)
 
 # Exploration des données fusionnées
 head(donnees_fusionnees)
